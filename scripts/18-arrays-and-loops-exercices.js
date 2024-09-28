088d09aa4d898eef337211e7d34b1fd9621fc8f3
Igor Oliveira lima <89111045+KingSP13@users.noreply.github.com>
2024-09-27 21:31:26 -0300

    stuck exercise, solved
    ep 11
    ex1 and ex2 18 arrays and loops

---
+++ scripts/18-arrays-and-loops-exercices.js
@@ -0,0 +1,37 @@
+//Ex 1
+
+/*
+
+const stringArray = ['hello', 'search', 'apple', 'search', 'monday']
+
+for(let i = 0; i < stringArray.length; i++) {
+    const thereIs = stringArray[i]
+    let position = [i]
+    if(thereIs === 'search') {
+        position = [i]
+        console.log(position)
+    } else if (i === stringArray.length-1 && position[i] != 'search') {
+        console.log(-1)
+    }
+}
+
+//Ex 2
+
+const stringArray = ['hello', 'search', 'apple', 'search', 'monday']
+
+for(let i = 0; i < stringArray.length; i++) {
+    const thereIs = stringArray[i]
+    let position = [i]
+    if(thereIs === 'search') {
+        position = [i]
+        console.log(position)
+        break
+    } else if (i === stringArray.length-1 && position[i] != 'search') {
+        console.log(-1)
+    }
+}
+
+*/
+
+//Ex 3
+
