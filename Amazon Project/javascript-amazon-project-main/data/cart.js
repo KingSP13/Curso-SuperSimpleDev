e4cb1ebdde3e1377d3188091495e8e426c0ef59b
Igor Oliveira lima <89111045+KingSP13@users.noreply.github.com>
2024-10-21 10:35:23 -0300

    cart features added, major adjustmentos to better visualization in the course
    cart features like localstorage, delete button made interactive, import and export variables between files for better organization.

--- Amazon Project/javascript-amazon-project-main/data/cart.js
+++ Amazon Project/javascript-amazon-project-main/data/cart.js
@@ -1,29 +1,50 @@
-const cart = [];
+export let cart = JSON.parse(localStorage.getItem('cart'));
 
-document.querySelectorAll('.add-to-cart-button')
-.forEach((button) => {
-    button.addEventListener('click', () => {
-        const productId = button.dataset.productId
-        let matchingItem;
-        cart.forEach((item) => {
-            if(productId === item.productId) {
-                matchingItem = item
-            }
-        })
+if (!cart) {
+  cart = [{
+    productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+    quantity: 2,
+  }, {
+    productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+    quantity: 1
+  }];
+}
 
-        if(matchingItem) {
-           matchingItem.quantity++ 
-        } else {
-            cart.push({
-                productId: productId,
-                quantity: 1
-            });
-        }
-        let cartQuantity = 0
+function saveToStorage() {
+  localStorage.setItem('cart', JSON.stringify(cart));
+}
 
-        cart.forEach((item) => {
-            cartQuantity += item.quantity
-        });
-        document.querySelector('.cart-quantity').innerHTML = cartQuantity
+export function addToCart(productId) {
+  let matchingItem;
+
+  cart.forEach((cartItem) => {
+    if (productId === cartItem.productId) {
+      matchingItem = cartItem;
+    }
+  });
+
+  if (matchingItem) {
+    matchingItem.quantity += 1;
+  } else {
+    cart.push({
+      productId: productId,
+      quantity: 1
     });
-});
+  }
+
+  saveToStorage();
+}
+
+export function removeFromCart(productId) {
+  const newCart = [];
+
+  cart.forEach((cartItem) => {
+    if (cartItem.productId !== productId) {
+      newCart.push(cartItem);
+    }
+  });
+
+  cart = newCart;
+
+  saveToStorage();
+}
