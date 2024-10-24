ae78552b61e0cedbf3a259a87a50c9eeb063801d
Igor Oliveira lima <89111045+KingSP13@users.noreply.github.com>
2024-10-24 18:46:52 -0300

    Cart features updated
    Auto refresh when changing the shipping method, updating delivery date, recursive method to update the cart when clicked, add dataset's to capture delivery methods, HTML code being generated dinamicaly according to the products

--- Amazon Project/javascript-amazon-project-main/data/cart.js
+++ Amazon Project/javascript-amazon-project-main/data/cart.js
@@ -43,13 +43,14 @@ function saveToStorage() {
 export function addToCart(productId) {
   let matchingItem;
 
-  let quantity = Number(document.querySelector(`.js-quantity-selector-${productId}`).value)
-
+  
   cart.forEach((cartItem) => {
     if (productId === cartItem.productId) {
       matchingItem = cartItem;
     }
   });
+  
+  let quantity = Number(document.querySelector(`.js-quantity-selector-${productId}`).value)
 
   if (matchingItem) {
     matchingItem.quantity += quantity;
@@ -76,6 +77,19 @@ export function removeFromCart(productId) {
   });
 
   cart = newCart;
+
+  saveToStorage();
+}
+
+export function updateDeliveryOption(productId, deliveryOptionId) {
+  let matchingItem;
+
+  cart.forEach((cartItem) => {
+    if (productId === cartItem.productId) {
+      matchingItem = cartItem;
+    }
+  });
+  matchingItem.deliveryOptionId = deliveryOptionId;
 
   saveToStorage();
 }
