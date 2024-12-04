5e98a3248d69f5a4e1b931d37e145929c4b0ddbd
Igor Oliveira lima <89111045+KingSP13@users.noreply.github.com>
2024-12-04 10:30:44 -0300

    API bringing the products instead of local file
    products.js changed, commentes all the products and they are loading through an API in 'supersimplebackend.dev/products', callback functions to load and render the pages, modified in checkout.js too

--- Amazon Project/javascript-amazon-project-main/scripts/checkout.js
+++ Amazon Project/javascript-amazon-project-main/scripts/checkout.js
@@ -1,7 +1,11 @@
 import {renderOrderSummary} from './checkout/orderSummary.js';
 import {renderPaymentSummary} from './checkout/paymentSummary.js';
+import {loadProducts} from '../data/products.js'
 // import '../data/cart-class.js'; utilizado para teste
-import '../data/backend-practice.js';
+// import '../data/backend-practice.js';
 
-renderOrderSummary();
-renderPaymentSummary();
+//Meu primeiro pensamento aqui era de passar ambas as funções como parâmetro, mais simples que a função anônima, porém nos parentesis vai o parâmetro formal, e como na declaração da função definimos 'fun', só pode receber um parâmetro e não faria muito sentido, a solução de função anônima foi perfeita por que uma função é um código reutilizável, aqui ela está sendo aplicada para executar o render da order page e da product page quando os produtos forem carregados da API.
+loadProducts(() => {
+    renderOrderSummary();
+    renderPaymentSummary();
+})
